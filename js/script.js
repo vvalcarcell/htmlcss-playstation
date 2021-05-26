8e7821ece8c13998e4f9591d0acbd5356d883d33
vvalcarcell <valcarcellvalentina@gmail.com>
2021-05-26 16:37:52 +0200

    js-experiments

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById('supply').innerHTML = '#Supply by JavaScript!';
+
+alert("Hack Attempt by JavaScript!!!");
+
+console.log('Keep Pandas Safe! Be Careful About What You Say!');
+
+document.writeln('JavaScript Foray Underway...');
+
+var x = 1;
+var y = 3;
+var pippo = x + y;
+
+document.getElementById('pippo').innerHTML = pippo;
+
+var x = 1;
+var z = 4;
+var pluto = x + z;
+
+document.getElementById('pluto').innerHTML = pluto;
